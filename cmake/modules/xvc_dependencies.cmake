fb4bb58a89eda16a769428763978334206ddc375
sum01 <sum01@protonmail.com>
2017-09-23 16:00:45 -0400

    Implement Crawler (vcashc) build ADD option to NOT build daemon (ON by default) ADD option to build crawler (OFF by default) ADD crawler lib for building vcashc (crawler binary) ADD install module to keep main CMakeLists.txt clean, and loop to make exports for all binaries EDIT xvc_dependencies module to ignore Berkeley DB if only building crawler EDIT travis.yml to build crawler along with daemon

--- cmake/modules/xvc_dependencies.cmake
+++ cmake/modules/xvc_dependencies.cmake
@@ -11,16 +11,32 @@ find_package(Boost "1.54.0" COMPONENTS system REQUIRED)
 find_package(OpenSSL "1.0.1" REQUIRED)
 set(OPENSSL_MAX_VER "1.0.2") # Used in max ver check
 
-# ~~ Berkeley DB ~~
-# Prevent accidental building with DB v5, which isn't compatible with wallets built with DB v6
-option(WITH_INCOMPATIBLE_BDB "Enables building with a Berkeley DB v5 minimum instead of v6 minimum." OFF)
-IF(WITH_INCOMPATIBLE_BDB)
-  set(BERKELEYDB_MIN_VER "5.0.0")
-ELSE()
-  set(BERKELEYDB_MIN_VER "6.0.0")
+# Max version check
+IF(OPENSSL_VERSION VERSION_GREATER ${OPENSSL_MAX_VER})
+  message(FATAL_ERROR "The detected OpenSSL v${OPENSSL_VERSION} isn't compatible! Maximum of v${OPENSSL_MAX_VER} is compatible.")
 ENDIF()
-set(BERKELEYDB_MAX_VER "6.1.36") # Last release ver before v6.2, which isn't compatible
-find_package(BerkeleyDB ${BERKELEYDB_MIN_VER} REQUIRED)
+
+# ~~ Berkeley DB ~~
+IF(BUILD_VCASH_DAEMON) # Only the daemon needs Berkeley DB
+  # Prevent accidental building with DB v5, which isn't compatible with wallets built with DB v6
+  option(WITH_INCOMPATIBLE_BDB "Enables building with a Berkeley DB v5 minimum instead of v6 minimum." OFF)
+  IF(WITH_INCOMPATIBLE_BDB)
+    set(BERKELEYDB_MIN_VER "5.0.0")
+  ELSE()
+    set(BERKELEYDB_MIN_VER "6.0.0")
+  ENDIF()
+  set(BERKELEYDB_MAX_VER "6.1.36") # Last release ver before v6.2, which isn't compatible
+  find_package(BerkeleyDB ${BERKELEYDB_MIN_VER} REQUIRED)
+
+  # Max version check
+  IF(BERKELEYDB_VERSION VERSION_GREATER "${BERKELEYDB_MAX_VER}")
+    message(FATAL_ERROR "The detected BerkeleyDB v${BERKELEYDB_VERSION} isn't compatible! Maximum of v${BERKELEYDB_MAX_VER} is compatible.")
+  # Throw a warning if the user has DB ver < 6 but continue building
+  ELSEIF(BERKELEYDB_VERSION VERSION_LESS "6.0.0")
+    message(WARNING "Pre-existing wallet data is not backwards compatible with version v5 of Berkeley DB if it was originally built with v6. \n
+      Read \"docs/BUILDING.md\" for more info.")
+  ENDIF()
+ENDIF()
 
 # ~~ Threads ~~
 IF(CMAKE_SYSTEM_NAME MATCHES "Windows")
@@ -33,16 +49,3 @@ ELSE()
   set(THREADS_PREFER_PTHREAD_FLAG ON)
 ENDIF()
 find_package(Threads REQUIRED) # pthread not needed specificly, code uses #include <thread>
-
-# ~~ Max version checks after finding ~~
-IF(OPENSSL_VERSION VERSION_GREATER ${OPENSSL_MAX_VER})
-  message(FATAL_ERROR "The detected OpenSSL v${OPENSSL_VERSION} isn't compatible! Maximum of v${OPENSSL_MAX_VER} is compatible.")
-ENDIF()
-
-IF(BERKELEYDB_VERSION VERSION_GREATER "${BERKELEYDB_MAX_VER}")
-  message(FATAL_ERROR "The detected BerkeleyDB v${BERKELEYDB_VERSION} isn't compatible! Maximum of v${BERKELEYDB_MAX_VER} is compatible.")
-# Throw a warning if the user has DB ver < 6 but continue building
-ELSEIF(BERKELEYDB_VERSION VERSION_LESS "6.0.0")
-  message(WARNING "Pre-existing wallet data is not backwards compatible with version v5 of Berkeley DB if it was originally built with v6. \n
-    Read \"docs/BUILDING.md\" for more info.")
-ENDIF()
