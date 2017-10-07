752a18330946bb3486b6405ac0fe5d07503fa72b
sum01 <sum01@protonmail.com>
2017-10-06 23:44:22 -0400

    Fix workaround for missing bin/lib dir vars Also changed how the executable names are handled, purely for simplicity.

--- cmake/modules/vcash_install.cmake
+++ cmake/modules/vcash_install.cmake
@@ -1,40 +1,34 @@
 # Installs the executable and rpc script
 
-# Set paths if none detected, as they don't seem to default correctly
-IF(NOT CMAKE_INSTALL_BINDIR)
-  set(CMAKE_INSTALL_BINDIR bin)
-ENDIF()
-IF(NOT CMAKE_INSTALL_LIBDIR)
-  set(CMAKE_INSTALL_LIBDIR lib)
-ENDIF()
+# Defines standardized defaults for install paths
+include(GNUInstallDirs)
 
-foreach(_exe_name ${_VCASH_EXE})
+foreach(_VCASH_INSTALL_TARGET ${_VCASH_INSTALL_TARGETS})
   # Gives "Make install" esque operations a location to install to...
   # and creates a .cmake files for other projects to import.
-  install(TARGETS ${_exe_name}
-    EXPORT "${_exe_name}-targets"
+  install(TARGETS ${_VCASH_INSTALL_TARGET}
+    EXPORT "${_VCASH_INSTALL_TARGET}-targets"
     RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}"
   )
 
   # "The install(TARGETS) and install(EXPORT) commands work together to install a target and a file to help import it"
   # Installs the Vcash.cmake file which external projects can import.
   # This will point towards the pre-compiled binary after installation, but be treated as if it had been compiled inside the external project.
-  install(EXPORT "${_exe_name}-targets"
+  install(EXPORT "${_VCASH_INSTALL_TARGET}-targets"
     DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/Vcash" # ex: /usr/lib/cmake/Vcash/Vcash-targets.cmake
   )
 
   # "The export command is used to generate a file exporting targets from a project build tree"
   # Creates an import file for external projects which are aware of the build tree.
   # This will not be installed, and is only usefull in certain situations.
-  export(TARGETS ${_exe_name}
-    FILE "${_exe_name}-exports.cmake"
+  export(TARGETS ${_VCASH_INSTALL_TARGET}
+    FILE "${_VCASH_INSTALL_TARGET}-exports.cmake"
   )
 endforeach()
 
 # Installs vcashrpc if not on Windows
-IF(CMAKE_SYSTEM_NAME MATCHES "Windows")
-  message(STATUS "The RPC script will not be installed because you are on a Windows machine.")
-ELSE()
+IF(NOT CMAKE_SYSTEM_NAME MATCHES "Windows")
+  message(STATUS "The vcashrpc script will be installed.")
   install(FILES vcashrpc
     PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE # Basically chmod 755
     DESTINATION "${CMAKE_INSTALL_BINDIR}"
@@ -48,6 +42,6 @@ ENDIF()
 #  set(_VCASH_RPC_NAME "vcashrpc")
 #ENDIF()
 #install(FILES ${_VCASH_RPC_NAME}
-#  PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_EXECUTE
+#  PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
 #  DESTINATION "${CMAKE_INSTALL_BINDIR}"
 #)
